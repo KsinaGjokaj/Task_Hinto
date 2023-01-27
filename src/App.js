ef4459d1885b3502fc5fb74967f7c97e56bee508
KsinaGjokaj <ksina.gjokaj2000@gmail.com>
2023-01-27 12:25:08 +0100

    firtCommit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import FetchData from "./components/FetchData";
+
+
+
+function App() {
+  return (
+    <div>
+      <FetchData />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/FetchData.js
@@ -0,0 +1,69 @@
+import React, { useEffect, useState, useMemo } from "react";
+import '../components/FetchData.css';
+import Pagination from "./Pagination";
+
+let PageSize = 10;
+
+const FetchData = () => {
+    const [data, setData] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    //pagination
+
+    const getApi = (_start) => {
+        fetch(`https://jsonplaceholder.typicode.com/posts?_start=${(_start - 1) * 10}&_limit=10`)
+            .then((response) => response.json())
+            .then((data) => setData(data));
+    }
+
+    useEffect(() => {
+        getApi(currentPage);
+
+    }, [currentPage])
+
+
+    console.log(`currentPage ${currentPage}`);
+    return (
+        <div >
+            <h1>Fetch dataa</h1>
+
+            <table className="table">
+                <tr>
+                    <th>ID</th>
+                    <th>USER ID</th>
+                    <th>TITLE</th>
+                </tr>
+                {data && data.length > 0 && data.map((item, index) => (
+                    <TableRow item={item} />
+                ))
+                }
+            </table>
+            <Pagination
+                currentPage={currentPage}
+                totalCount={100}
+                pageSize={PageSize}
+                onPageChange={page => setCurrentPage(page)}
+            />
+        </div >
+    );
+}
+const TableRow = ({ item }) => {
+    const [desc, setDesc] = useState();
+    return (
+        <tr key={item.id} onClick={() => {
+            setDesc(item.body)
+        }}>
+
+            <td>{item.id}</td>
+            <td>{item.userId}</td>
+            <td className="title">
+                <a href="#">{item.title}</a>
+                <div className="description">
+                    {desc}
+                </div>
+            </td>
+        </tr>
+    );
+
+}
+
+export default FetchData;
